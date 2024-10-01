5e7f0c6303c091be3a469c8109f2af90c36e5ba9
Akshat Vishwakarma <a.vishwakarma2110@gmail.com>
2024-10-01 13:01:22 +0530

    refactor

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const crypto = require('crypto');
-const axios = require('axios');
-
+const express = require("express");
+const cors = require("cors");
+const crypto = require("crypto");
+const axios = require("axios");
 
 const app = express();
 
@@ -10,89 +9,73 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
-    res.send("API is running");
-})  
+  res.send("API is running");
+});
 
+let merchant_id = "PGTESTPAYUAT";
+let salt_key = "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399";
 
-let salt_key = '96434309-7796-489d-8924-ab56988a6076'
-let merchant_id = 'PGTESTPAYUA86'  
+app.post("/order", async (req, res) => {
+  try {
+    let { MUID, transactionId, amount, mobile, name } = req.body;
 
-app.post('/order', async (req, res) => {
+    const data = {
+      merchantUserId: MUID,
+      merchantId: merchant_id,
+      merchantTransactionId: transactionId,
+      name: name,
+      amount: amount * 100,
+      redirectUrl: `http://localhost:8000/success/status?id=${transactionId}`,
+      redirectMode: "POST",
+      mobileNumber: mobile,
+      paymentInstrument: {
+        type: "PAY_PAGE",
+      },
+    };
 
-    try{
+    const keyIndex = 1;
 
+    const payload = JSON.stringify(data);
+    const payloadMain = Buffer.from(payload).toString("base64");
 
-        let {
-            MUID,
-            transactionId,
-            amount,
-            mobile,
-            name
-        } = req.body;
+    const string = payloadMain + "/pg/v1/pay" + salt_key;
 
+    const sha256 = crypto.createHash("sha256").update(string).digest("hex");
 
-        const data = {
-            merchantId: merchant_id,
-            merchantTransactionId: transactionId,
-            name: name,
-            amount: amount * 100,
-            redirectUrl: `http://localhost:8000/success/status?id=${transactionId}`,
-            redirectMode: 'POST',
-            mobileNumber: mobile,
-            paymentInstrument: {
-                type: "PAY_PAGE"
-            }
-        }
+    const checksum = sha256 + "###" + keyIndex;
 
+    // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+    const prod_URL =
+      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
 
-        const keyIndex = 1
+    const options = {
+      method: "POST",
+      url: prod_URL,
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        "X-VERIFY": checksum,
+      },
+      data: {
+        request: payloadMain,
+      },
+    };
 
-        const payload = JSON.stringify(data);
-        const payloadMain = Buffer.from(payload).toString('base64');
-
-        const string = payloadMain + '/pg/v1/pay' + salt_key;
-
-        const sha256 = crypto.createHash('sha256').update(string).digest('hex');
-
-        const checksum = sha256 + '###' + keyIndex;
-
-        // const prod_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
-        const prod_URL = "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
-
-        const options = {
-            method: "POST",
-            url: prod_URL,
-            headers:{
-                'accept': "application/json",
-                "content-type": "application/json",
-                "X-VERIFY": checksum
-            },
-            data:{
-                request: payloadMain
-            }
-        }
-
-        await axios(options).then(response => {
-            res.json(response.data)
-        }).catch(error => {
-            console.log(error.message);
-            res.status(500).json({error: error.message})
-        })
-
-
-
-
-    } catch(error){
+    await axios(options)
+      .then((response) => {
+        res.json(response.data);
+      })
+      .catch((error) => {
         console.log(error);
-    }
-})
-
-
-
+        res.status(500).json({ error: error.message });
+      });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 //listen server
-app.listen(8000, ()=>{
+app.listen(8000, () => {
   console.log("Server is running on port 8000");
-})
+});
